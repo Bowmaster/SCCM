57e6af25df594f28f255bf3f19eec1276459dd4e
Ben J. Lutterbach <blutterb@bbl.blutterbach.com>
2017-11-07 15:14:10 -0500

    Wrote Script

---
+++ Client Operations/Uninstall-Client.ps1
@@ -0,0 +1,59 @@
+#Requires -RunAsAdministrator
+
+param(
+    # Opts to perform a complex, scripted uninstall rather than just using ccmsetup with the uninstall flag
+    [Parameter(Mandatory=$false)]
+    [switch]
+    $ComplexRemoval
+)
+
+try {
+    if ($ComplexRemoval) {
+        $CmServices = @("ccmsetup",
+        "ccmexec",
+        "cmrcservice",
+        "smstsmgr")
+        $CmServices | Stop-Service -ErrorAction SilentlyContinue
+        $Dependcies = (Get-Service -Name winmgmt).DependentServices
+        $WmiBackupPath = "C:\Windows\Temp\WmiBackup$((Get-Date).ToFileTime()).wmi"
+        winmgmt /backup $WmiBackupPath
+        Write-Host "The WMI repository has been backup up to $WmiBackupPath" -ForegroundColor Yellow
+        Stop-Service winmgmt -Force -ErrorAction Stop
+        $CmServices | % {sc.exe delete $_}
+
+        @("HKLM:\SYSTEM\CurrentControlSet\services\Ccmsetup",
+        "HKLM:\SYSTEM\CurrentControlSet\services\CcmExec",
+        "HKLM:\SYSTEM\CurrentControlSet\services\smstsmgr",
+        "HKLM:\SYSTEM\CurrentControlSet\services\CmRcService",
+        "HKLM:\SOFTWARE\Microsoft\CCM",
+        "HKLM:\SOFTWARE\Microsoft\CCMSetup",
+        "HKLM:\SOFTWARE\Microsoft\SMS",
+        "C:\Windows\CCM",
+        "C:\Windows\ccmsetup",
+        "C:\Windows\ccmcache",
+        "C:\Windows\SMSCFG.ini",
+        "C:\Windows\SMS*.mif") | % {Remove-Item $_ -Force -Recurse -ErrorAction SilentlyContinue}
+
+        Unregister-ScheduledTask -TaskPath "\Microsoft\Configuration Manager\" -TaskName * -Confirm:$false
+
+        get-wmiobject -query "SELECT * FROM __Namespace WHERE Name='CCM'" -Namespace "root" | Remove-WmiObject
+
+        get-wmiobject -query "SELECT * FROM __Namespace WHERE Name='sms'" -Namespace "root\cimv2" | Remove-WmiObject
+
+        Start-Service Winmgmt
+        
+        $Dependcies | % {
+            if ($_.Status -eq "Running") {
+                Start-Service $_ 
+            }
+        }
+        Write-Host "The process completed without a STOPPING error. Please confirm the health of server before reinstalling SCCM." -ForegroundColor Green
+    }else {
+        Write-Warning "Note: ccmsetup commands return to the console immediately while processing continues in the background, please verify by monitoring the ccmsetup.log file for a successful return code."
+        C:\Windows\ccmsetup\ccmsetup.exe /uninstall
+    }
+}
+catch {
+    Write-Warning "This following errors occured:"
+    Write-Error $_
+}
